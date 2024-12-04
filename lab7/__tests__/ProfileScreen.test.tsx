17868244cf43c8027a1b041bbcb08c9c6d400206
Edwin Garcés <edwingarcessaucedo@gmail.com>
2024-12-03 19:35:05 -0600

    test: tests fixed

--- lab7/__tests__/ProfileScreen.test.tsx
+++ lab7/__tests__/ProfileScreen.test.tsx
@@ -1,8 +1,15 @@
-// __tests__/ProfileScreen.test.tsx
 import React from 'react';
 import renderer from 'react-test-renderer';
 import ProfileScreen from '../src/screens/ProfileScreen';
 import {expect, it} from '@jest/globals';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: jest.fn(() => ({
+      navigate: jest.fn(),
+    })),
+  };
+});
 
 it('ProfileScreen component renders correctly', () => {
   const tree = renderer.create(<ProfileScreen />).toJSON();
