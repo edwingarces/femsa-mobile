97674ec2ad7179b71168170e6e5f27f9f7f294fe
Edwin Garcés <edwingarcessaucedo@gmail.com>
2024-12-10 11:05:47 -0600

    feat: lab8

---
+++ lab8/ios/NativeBatteryModule/RCTNativeBatteryModule.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeBatteryModule.h
+//  MobileCourse
+//
+//  Created by Edwin Garcés Saucedo on 09/12/24.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeBatteryModule/NativeBatteryModule.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeBatteryModule : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
